697fdfa1eb66cb9465e58b2d35be4cdb44e959c2
maorbrantz <maorba0120@gmail.com>
2024-08-15 20:24:58 +0300

    Updating terraform files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,4 +1,13 @@
-# VPC Configuration
+#-----------------------------------
+# Pac-Man Project
+# Created by Maor Brantz
+#-----------------------------------
+
+
+
+#--------------- VPC ---------------
+
+# Create VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
@@ -7,6 +16,7 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Create Subnet A
 resource "aws_subnet" "subnet_a" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
@@ -17,6 +27,7 @@ resource "aws_subnet" "subnet_a" {
   }
 }
 
+# Create Subnet B
 resource "aws_subnet" "subnet_b" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
@@ -27,19 +38,10 @@ resource "aws_subnet" "subnet_b" {
   }
 }
 
-# Cluster Configuration
-resource "aws_eks_cluster" "cluster" {
-  name     = var.cluster_name
-  role_arn  = aws_iam_role.eks.arn
-  version   = "1.21"
+#---------------EKS---------------
 
-  vpc_config {
-    subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
-  }
-}
-
-# IAM Role Configuration (for the cluster)
-resource "aws_iam_role" "eks" {
+# IAM Role for EKS
+resource "aws_iam_role" "eks_role" {
   name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
@@ -56,26 +58,24 @@ resource "aws_iam_role" "eks" {
   })
 }
 
+# Add Policy to IAM Role for EKS
 resource "aws_iam_role_policy_attachment" "eks_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role     = aws_iam_role.eks.name
+  role     = aws_iam_role.eks_role.name
 }
 
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids      = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+# Cluster Configuration
+resource "aws_eks_cluster" "cluster" {
+  name     = var.cluster_name
+  role_arn  = aws_iam_role.eks_role.arn
+  version   = "1.21"
 
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+  vpc_config {
+    subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
   }
-
-  instance_types = ["t3.medium"]
 }
 
+# IAM Role for Nodes
 resource "aws_iam_role" "node_role" {
   name = "eks-node-role"
 
@@ -93,15 +93,43 @@ resource "aws_iam_role" "node_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "node_policy" {
+# Add Policy to IAM Role for Nodes
+resource "aws_iam_role_policy_attachment" "node_role-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role     = aws_iam_role.node_role.name
 }
 
-resource "aws_iam_role_policy_attachment" "node_cni_policy" {
+resource "aws_iam_role_policy_attachment" "node_role-AmazonEKSCNIPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSCNIPolicy"
   role     = aws_iam_role.node_role.name
 }
+
+resource "aws_iam_role_policy_attachment" "node_role-AmazonEC2ContainerRegistryReadOnlyy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role     = aws_iam_role.node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_role-AmazonSSMManagedInstanceCore" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role     = aws_iam_role.node_role.name
+}
+
+# Create Node Group
+resource "aws_eks_node_group" "worker_nodes_group" {
+  cluster_name    = aws_eks_cluster.cluster.name
+  node_group_name = "${var.cluster_name}-worker-nodes-group"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+
+  scaling_config {
+    desired_size = var.scaling-desired_nodes
+    max_size     = var.scaling-max_nodes
+    min_size     = var.scaling-desired_nodes
+  }
+
+  instance_types = ["t3.medium"]
+}
+
 
 # MongoDB (Using DocumentDB)
 resource "aws_docdb_cluster" "mongodb" {
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,15 +1,34 @@
-# variables.tf
+# Main Variables
 variable "region" {
   description = "AWS region to deploy resources"
   type        = string
   default     = "us-west-2"
 }
 
+
+# EKS
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
 }
 
+
+# EKS - Nodes Variables
+variable "scaling-desired_nodes" {
+  description = "Number of Instances in the Node Group"
+  type        = string
+  default     = "2"
+
+}  
+
+variable "scaling-max_nodes" {
+  description = "Maximum Number of Instance in the Node Group"
+  type        = string
+  default     = "4"
+}
+
+
+# MongoDB Variables
 variable "mongodb_cluster_identifier" {
   description = "Identifier for the MongoDB cluster"
   type        = string
