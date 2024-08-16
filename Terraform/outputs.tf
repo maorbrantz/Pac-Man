83d181f86d9829e0e1650a10a5e52d63465b4623
maorbrantz <maorba0120@gmail.com>
2024-08-16 17:35:21 +0300

    Updating files

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,10 +1,10 @@
 # Outputs
 output "eks_cluster_endpoint" {
   description = "The endpoint of the EKS cluster"
-  value       = aws_eks_cluster.cluster.endpoint
+  value       = aws_eks_cluster.Cluster.endpoint
 }
 
 output "mongodb_endpoint" {
   description = "The endpoint of the MongoDB cluster"
-  value       = aws_docdb_cluster.mongodb_cluster.endpoint
+  value       = aws_docdb_cluster.MongoDB_Cluster.endpoint
 }
